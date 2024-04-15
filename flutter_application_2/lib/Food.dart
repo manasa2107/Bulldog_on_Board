2ae708973eda8fe190caa35b511486c59abd7fd3
YAGNA MANASA BOYAPATI <82016723+manasa2107@users.noreply.github.com>
2024-04-15 00:20:50 -0500

    Update Food.dart
    Updated navBar

--- flutter_application_2/lib/Food.dart
+++ flutter_application_2/lib/Food.dart
@@ -153,7 +153,7 @@ class Food extends StatelessWidget {
                 padding: EdgeInsets.only(
                     left: 10, right: 10), // Adjust left and right margins here
                 child: IconButton(
-                  icon: Icon(Icons.home,
+                  icon: Icon(Icons.apartment,
                       size: 45,
                       color: const Color.fromRGBO(
                           152, 3, 3, 0.929)), // Customize size and color here
@@ -187,7 +187,7 @@ class Food extends StatelessWidget {
                 padding: EdgeInsets.only(
                     left: 10, right: 10), // Adjust left and right margins here
                 child: IconButton(
-                  icon: Icon(Icons.flight,
+                  icon: Icon(Icons.directions_bus,
                       size: 40,
                       color: const Color.fromRGBO(
                           152, 3, 3, 0.929)), // Customize size and color here
