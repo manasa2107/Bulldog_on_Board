d0059485e3c60b0aa5ef04b6de6426a8534ccbc0
YAGNA MANASA BOYAPATI <82016723+manasa2107@users.noreply.github.com>
2024-04-15 00:57:33 -0500

    Update Travel.dart

--- flutter_application_2/lib/Travel.dart
+++ flutter_application_2/lib/Travel.dart
@@ -113,7 +113,7 @@ class Travel extends StatelessWidget {
                         // fontWeight: FontWeight.bold,
                         color: Color.fromARGB(255, 250, 233, 83)),
                   ),
-                  Icon(Icons.info, color: Color.fromARGB(255, 250, 233, 77)),
+                  Icon(Icons.directions_bus, color: Color.fromARGB(255, 250, 233, 77), size:30),
                 ],
               ),
               SizedBox(),
@@ -142,7 +142,7 @@ class Travel extends StatelessWidget {
       Padding(
         padding: EdgeInsets.only(left: 10, right: 10), // Adjust left and right margins here
         child: IconButton(
-          icon: Icon(Icons.home, size: 45, color: const Color.fromRGBO(152, 3, 3, 0.929)),  // Customize size and color here
+          icon: Icon(Icons.apartment, size: 45, color: const Color.fromRGBO(152, 3, 3, 0.929)),  // Customize size and color here
           onPressed: () {
             // Handle sign up action
             Navigator.push(
@@ -168,7 +168,7 @@ class Travel extends StatelessWidget {
       Padding(
         padding: EdgeInsets.only(left: 10, right: 10), // Adjust left and right margins here
         child: IconButton(
-          icon: Icon(Icons.flight, size: 40, color: const Color.fromRGBO(152, 3, 3, 0.929)),  // Customize size and color here
+          icon: Icon(Icons.directions_bus, size: 40, color: const Color.fromRGBO(152, 3, 3, 0.929)),  // Customize size and color here
           onPressed: () {
             // Handle contact us action
             Navigator.push(
