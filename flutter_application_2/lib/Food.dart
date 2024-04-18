f5d5d5119ccddfdd573d24a9f8b0849606d3f77c
HarikaYarly <149696557+HarikaYarly@users.noreply.github.com>
2024-04-18 16:12:35 -0500

    Update Food.dart: Settings Page Linked

--- flutter_application_2/lib/Food.dart
+++ flutter_application_2/lib/Food.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/Food2.dart';
+import 'package:flutter_application_2/settings.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -86,7 +87,8 @@ class Food extends StatelessWidget {
                 //leading: Icon(Icons.settings),
                 title: Text('Settings'),
                 onTap: () {
-                  // Navigate to settings screen or perform action
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()));
                 },
               ),
               ListTile(
